0758bb903af320414c39ff131f9577f3a2aac87e
Dereck Castillo <dereckurielcastilloramirez@gmail.com>
2026-01-18 23:00:47 -0600

    Add files via upload

---
+++ Dia 3 - JS/TiposDeDatos/Variables/code.js
@@ -0,0 +1,12 @@
+let miVariable;
+miVariable = "Hola, soy una variable" ;
+
+let miArray = [1, 2, 3, 4, 5];
+
+let miLista = ["Hola", 42, true, 3.14];
+
+alert(miVariable);
+
+alert(miArray);
+
+alert(miLista[2]);
